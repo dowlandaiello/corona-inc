ace2ffcceeac5cc12c0f41cfeb5ef18d8c7157a1
Dowland Aiello <mitsukomegumii@gmail.com>
2020-03-16 20:00:53 -0400

    coronainc: Batch commit.
    Signed-off-by: Dowland Aiello <mitsukomegumii@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Dashboard api-endpoint="https://coronavirus-19-api.herokuapp.com"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from './components/Dashboard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Dashboard
   }
 }
 </script>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+	<p>lulw</p>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+	name: 'Dashboard',
+	props: ['api-endpoint'],
+	data() {
+		const infectionsByCountr = fetch(`${this.apiEndpoint}/countries`)
+			.then(response => response.json())
+			.then(countryData => console.log(countryData));
+
+		return {};
+	},
+};
+</script>
